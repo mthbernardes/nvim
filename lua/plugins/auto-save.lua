f2f75b59c635244e8e86edf4fad9f5ca1225d589
Matheus Bernardes <12648924+mthbernardes@users.noreply.github.com>
2024-04-28 13:31:13 -0300

    update auto-save

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -1,5 +1,10 @@
 return {
   'okuuva/auto-save.nvim',
+  enabled = true,
   cmd = 'ASToggle',
-  event = { 'InsertLeave', 'TextChanged' },
+  trigger_events = { -- See :h events
+    immediate_save = { 'BufLeave', 'FocusLost' }, -- vim events that trigger an immediate save
+    defer_save = { 'InsertLeave', 'TextChanged' }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
+    cancel_defered_save = { 'InsertEnter' }, -- vim events that cancel a pending deferred save
+  },
 }
